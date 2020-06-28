b2f56012dc1f3c207092c6aeaf7bc4b37e880669
Michael Hidalgo <michael.hidalgo@owasp.org>
2020-06-28 11:30:24 -0600

    Quick refactoring so it supports multiple schemas

--- parser.py
+++ parser.py
@@ -1,26 +1,33 @@
 import xml.etree.ElementTree as ET
-import xml.dom.minidom
+import xml.dom.minidom, os
 from xml.etree import ElementTree
-import os
+from os import walk
 
 
 def parse_sysmon_events():
     print("+ Extracting XML ")
-    tree = ET.parse('./data/schema.xml')
-
-    root = tree.getroot()
-    
-    for event in root.findall('./events/event'):
-        event_content = ElementTree.tostring(event, encoding="utf-8", method="xml").decode()
-        event_name    = "[ " + event.get("value") + " ] - "  + event.get("name");
-        print("Parsing " + event_name)
-        save_xml(event_name, event_content)
+    for root, dirs, files in os.walk("schemas"):
+        for filename in files:
+            
+            
+            file_path                   = "schemas" + "/" + filename
+            file_name_without_extension = os.path.splitext(os.path.basename(filename))[0]
+            
+            tree = ET.parse(os.path.realpath(file_path))
+            root = tree.getroot()
+            
+            for event in root.findall('./events/event'):
+                event_content = ElementTree.tostring(event, encoding="utf-8", method="xml").decode()
+                event_name    = "[ " + event.get("value") + " ] - "  + event.get("name");
+                print("Parsing " + event_name)
+                save_xml(file_name_without_extension, event_name, event_content)
     print("+ Done")
 
-def save_xml(name, content):
+def save_xml(source, name, content):
     content_as_xml     = xml.dom.minidom.parseString(content).toprettyxml()
     xml_string         = os.linesep.join([s for s in content_as_xml.splitlines() if s.strip()]) # remove the weird newline issue
-    dest_folder        = "./events/"
+    dest_folder        = "./events/" + source
+    print("Destination" + dest_folder)
     target_file        = name + ".xml"
     fw = open(os.path.join(dest_folder, target_file), "w")
     fw.write(xml_string)
